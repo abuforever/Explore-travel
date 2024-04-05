d7ef83b303695482243bb282dc378aa4e79f9b07
MICROSOFT N <microsoftn@MacBook-Pro-MACBOOK.local>
2024-04-05 17:11:59 +0500

    Azizbek

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-// import React, {useEffect, useMemo } from "react";
-// import { useDispatch } from "react-redux";
-// import { setLang } from "./redux/lang";
-// import { useStoreState } from "./redux/selector";
-// import locale from "./localisation/locale.json";
+
 
 import React from "react";
 import Section from "./Components/Section";
@@ -10,21 +6,6 @@ import Navbar from "./Components/Navbar";
 import Trending from "./Components/Trending";
 
 function App() {
-  // const states = useStoreState();
-  // const dispatch = useDispatch();
-  // const langData = useMemo(() => locale[states.lang], [states.lang]);
-
-
-  {/* <h1>{langData.name}</h1>
-
-      <button onClick={() => dispatch(setLang("uz"))}>UZ</button>
-``      <button onClick={() => dispatch(setLang("en"))}>En</button> */}
-
-
-  // useEffect(() => {
-  //   document.title = langData.document_title;
-  // }, [langData.document_title]);
-
   return (
     <div className="App">
       <Navbar/>
